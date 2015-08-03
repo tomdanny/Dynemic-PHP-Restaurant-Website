4bf5559deff37888b221ec0b46c57cae3ecf05d6
Daniel Tomic <tom_danny2003@yahoo.com>
2015-08-03 11:43:17 -0400

    Templating global header and footer

--- index.php
+++ index.php
@@ -1,67 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <link href="/assets/styles.css" rel="stylesheet">
-</head>
-<body id="final-example">
+<?php 
+  include('includes/header.php');
+?>
 
-  <div class="wrapper">
 
-    <div id="banner">
-      <a href="/" title="Return to Home">
-        <img src="img/banner.png" alt="Franklin's Fine Dining">
-      </a>  
-    </div><!-- banner -->
 
-    <div id="nav">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="team.php">Team</a></li>
-        <li><a href="menu.php">Menu</a></li>
-        <li><a href="contact.php">Contact</a></li>
-      </ul>
-    </div><!-- nav -->
-
-    <div class="content">
-
-      <div id="footer" class="cf">
-
-        <div class="column three">
-          <strong>Phone</strong>
-          808.529.3819
-        </div><!-- column -->
-
-        <div class="column three">
-          <strong>Location</strong>
-          123 Kapiolani Boulevard<br>
-          Honolulu, HI
-        </div><!-- column -->
-
-        <div class="column three last">
-          <strong>Hours</strong>
-          <em>Tuesday - Thursday</em><br>
-          1:00pm - 9:00pm<br><br>
-
-          <em>Friday - Saturday</em><br>
-          4:00pm - 11:00pm<br><br>
-
-          <em>Sunday - Monday</em><br>
-          Closed<br><br>
-
-        </div><!-- column -->
-        
-      </div><!-- footer -->
-
-      <small>&copy;2015 Daniel Tomic</small>
-      
-    </div><!-- content -->
-    
-  </div><!-- wrapper -->
-
-  <div class="copyright-info">
-    <?php include(); ?>
-  </div><!-- copyright-info -->
-  
-</body>
-</html>
+<?php 
+  include('includes/footer.php');
+?>
